b1bcc6d3c1fa7f27b6ac2a87b266895accd378ce
BSamayoaM <bsamayoam@toluca.tecnm.mx>
2022-03-12 11:02:58 -0600

    Agregados metodos para unir y crear sockets

--- servidor.py
+++ servidor.py
@@ -7,4 +7,28 @@ def crear_socket():
 		global host
 		global puerto
 		global s
-		
+		host = ''
+		puerto = 9999
+		s = socket.socket()
+	except socket.error as mensaje:
+		print('Error en la creacion del socket' + str(mensaje))
+
+
+def unir_socket():
+	try:
+		global host
+		global puerto
+		global s
+		print("Uniendo el socket al puerto " + str(puerto))
+		s.bind((host,puerto))
+		s.listen(5)
+	except socket.error as mensaje:
+		print('Error en la union del socket' + str(mensaje))
+		print('Reintentando . . .')
+		unir_socket()
+
+def aceptar_socket():
+	conexion, direccion = s.accept()
+	print("Conexion establecida | "+" IP " + direccion[0] + "| PUERTO " + str(direccion[1]))
+	mandar_comandos(conexion)
+	conexion.close()
